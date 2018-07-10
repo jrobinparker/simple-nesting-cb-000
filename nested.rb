3638e892ef1c3abdcaf4ed0e8ef8280d639db8a2
Jeremy Parker <jrobinparker@gmail.com>
2018-07-10 03:33:34 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -144,6 +144,6 @@ def adding_to_dennis
      }
      
      programmer_hash[:dennis_ritchie][:languages] = "Assembly"
-     programmer_hash[:dennis_ritchie][:languages]
+     return programmer_hash[:dennis_ritchie][:languages]
 
 end
