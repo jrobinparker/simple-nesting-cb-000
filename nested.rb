5330bb78d419f0e03a77ead3b52eae4a21d9004d
Jeremy Parker <jrobinparker@gmail.com>
2018-07-10 03:32:45 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -1,4 +1,4 @@
-
+require "pry"
 def hopper
 	programmer_hash = 
  		{
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +37,9 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+  programmer_hash[:alan_kay][:known_for]
+     
 end
 
 def dennis_ritchies_language
@@ -55,6 +58,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+  programmer_hash[:dennis_ritchie][:languages].join
+  
 end
 
 def adding_matz
@@ -80,7 +86,14 @@ def adding_matz
         }
      }
 
-    
+    matz = {
+      :yukihiro_matsumoto => {
+        :known_for => "Ruby",
+        :languages => ["LISP", "C"]
+      }
+    }
+      
+
 end
 
 def changing_alan
@@ -102,7 +115,15 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+     programmer_hash.each do |person, details_hash|
+      details_hash.each do |attribute, data|
+        if attribute == :known_for 
+          data.delete {|known| known == "Object Orientation"}
+        end
+      end 
+    end
+    
+    programmer_hash[:alan_kay][:known_for] = alans_new_info
 end
 
 def adding_to_dennis
@@ -121,5 +142,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:dennis_ritchie][:languages] = "Assembly"
+     programmer_hash[:dennis_ritchie][:languages]
 
 end
